8d2f61c921ecc8a22dc4597171b12430a732f288
Marta Khromova <71217056+martetz@users.noreply.github.com>
2021-06-16 15:26:42 +0300

    the material-ui library added, two text fields from this library are added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import TextField from '@material-ui/core/TextField';
+
+
+
+
 function App() {
   return (
-    <div>Конвертер</div>
+    <div className='container'>
+      <h2>Конвертер</h2>
+      
+      <div className='card'>
+      <TextField
+          className='binary'
+          id="outlined-secondary"
+          label="Двочиная"
+          variant="outlined"
+          color="secondary"
+      />
+
+      <TextField
+          className='decimal'
+          id="outlined-secondary"
+          label="Десятичная"
+          variant="outlined"
+          color="primary"
+      />
+      </div>  
+  </div>
   );
 }
 
